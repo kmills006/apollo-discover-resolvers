5b35e8a5c6ecae526446fc919d06efbd802d2fad
Kristy Miller <kristy@poetic.io>
2018-10-08 11:23:22 -0500

    feat(discover): recursively search for resolver files

--- src/discoverResolvers.ts
+++ src/discoverResolvers.ts
@@ -1,6 +1,37 @@
+import appRootPath from 'app-root-path';
+import fs from 'fs';
+import glob from 'glob';
+import path from 'path';
+
 declare module 'apollo-discover-resolvers';
 
+// TODO: Types
+const flattenDeep = (arr: any): any => Array.isArray(arr)
+  ? arr.reduce((a: any, b: any) => a.concat(flattenDeep(b)) , [])
+  : [arr];
+
+const deepSearchResolverDirectory = (file: string): any => {
+  const fileStat = fs.lstatSync(file);
+
+  console.log('file: ', file);
+
+  if (!fileStat.isDirectory()) {
+    return file;
+  }
+
+  const children = glob.sync(`${file}/*`);
+
+  return children.map(child => deepSearchResolverDirectory(child));
+};
+
 export const discoverResolvers = (directory: string) => {
-  console.log('discoverResolvers');
-  console.log('directory: ', directory);
+  const resolverPath = `${appRootPath.path}/${path.relative(appRootPath.path, directory)}`;
+
+  const resolverFiles = flattenDeep(
+    glob
+      .sync(`${resolverPath}/*/*`)
+      .map(file => deepSearchResolverDirectory(file)),
+  );
+
+  console.log('resolverFiles', resolverFiles);
 };
